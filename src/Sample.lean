3e55b200a33f387f722dd9e0f04448027478189d
Siegmentation <siegmentationfault@yandex.ru>
2020-04-11 14:53:52 +0700

    https://github.com/leanprover/lean4/commit/746615d81dace4ae961630687e80db3ac67b22f1

--- src/Sample.lean
+++ src/Sample.lean
@@ -7,13 +7,13 @@ def echoProto : Proto :=
   res := Result,
   req := Req,
   nothing := Result.ok,
-  proto := λ p ⇒ match p with
-    | Msg.text s ⇒ some s
-    | _ ⇒ none }
+  proto := λ p => match p with
+    | Msg.text s => some s
+    | _ => none }
 
 def echo : echoProto.ev → echoProto.res
-| none ⇒ Result.ok
-| some s ⇒ Result.reply (Msg.text s)
+| none => Result.ok
+| some s => Result.reply (Msg.text s)
 
 def router (cx : Cx echoProto) : Cx echoProto :=
 ⟨cx.req, echo⟩
